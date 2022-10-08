b0b1d27acc18aedd9fc5b59ee93a1d3fdf29d38f
Shaladdin <abyboyganteng@gmail.com>
2022-10-08 20:39:45 +0700

    Untracked files issue resolved to fix .gitignore

--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
 "use strict";
 
-require('dotenv').config({ path: __dirname + '\\.env' });
-require('dotenv').config({ path: __dirname + '\\sslcert\\.env' });
-
-
+const WebSocket = require('ws');
 const Fs = require('fs');
+const https = require('https');
 const express = require('express');
-const https = require('http');
-const port = 3000;
 
+require('dotenv').config({ path: __dirname + '\\.env' });
+// require('dotenv').config({ path: __dirname + '\\sslcert\\.env' });
+
+const port = 8089;
 
 const app = express();
 const server = https.createServer({
-    ssl: true,
     key: Fs.readFileSync(`${__dirname}${process.env.KEY}`),
     cert: Fs.readFileSync(`${__dirname}${process.env.CERT}`)
 }, app);
 
-const WebSocket = require('ws');
-const { WebSocketServer } = WebSocket;
-const wss = new WebSocketServer({ server: server });
-
-
+const wss = new WebSocket.Server({ server });
 
 
 wss.on('connection', (ws) => {
@@ -33,5 +28,4 @@ wss.on('connection', (ws) => {
 })
 
 app.use(express.static(`${__dirname}/public`));
-
 server.listen(port, () => { console.log(`server is up on port ${port}!`); })
