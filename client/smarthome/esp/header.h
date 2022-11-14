e876ee30b6cb4280a2b190c237ed665460909176
Shaladdin <abyboyganteng@gmail.com>
2022-11-14 19:57:49 +0700

    esp file pins.h fix

--- client/smarthome/esp/header.h
+++ client/smarthome/esp/header.h
@@ -1,2 +1,14 @@
 #include <ArduinoJson.h>
-#include "pins.h"
+
+// outputs pin
+#define buzzer D0
+#define lamp D3
+#define fan D4
+const int outputs[] = {lamp, fan, buzzer};
+
+// ir
+#define irDalam D5
+#define irLuar D6
+const int inputs[] = {irDalam, irLuar};
+
+void pinInit();
--- client/smarthome/esp/pins.cpp
+++ client/smarthome/esp/pins.cpp
@@ -1,13 +1,4 @@
-// outputs pin
-#define buzzer D0
-#define lamp D3
-#define fan D4
-const int outputs[] = {lamp, fan, buzzer};
-
-// ir
-#define irDalam D5
-#define irLuar D6
-const int inputs[] = {irDalam, irLuar};
+#include "header.h"
 
 void pinInit()
 {
